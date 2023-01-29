68feebde651d7e5d69de5cfe12bdbb610f5dff95
RadioactiveOmega <Argameing@ya.ru>
2023-01-29 20:53:59 +0300

    Start

---
+++ src/main/java/ru/alexey/example/Models/Event.java
@@ -0,0 +1,8 @@
+package ru.alexey.example.Models;
+
+public class Event {
+    String title;
+    String description;
+    // дата
+    int periodicity;
+}
---
+++ src/main/java/ru/alexey/example/config/SpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package ru.alexey.example.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    /*
+    * Если в классе возникает ошибка java: cannot access jakarta.servlet.ServletException class file for jakarta.servlet.ServletException not found,
+    * переписать зависимость в pom.xml с Java Servlet API на Jakarta Servlet.
+    * */
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
